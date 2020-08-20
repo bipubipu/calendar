a67af3adb4515ae58470683dbb1766c0bbfed708
bipubipu <wyqatm@gmail.com>
2020-08-20 17:02:18 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,38 @@
-const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
 const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
-// First draft, will do data verification
-let year = prompt('Which year do you want to see?');
-let month = prompt('Which month do you want to see?');
+createCalendar(document.body, 2020, 7);
 
-createCalendar(document.body, year, month - 1);
+function createCalendar(elem, year, mon) {
+    let table = '<table><tr><th>MON</th><th>Tues</th><th>WED</th><th>THU</th><th>FRI</th><th>SAT</th><th>SUN</th></tr><tr>';
 
-function createCalendar(elem, year, month) {
-    let h2 = document.createElement('h2');
-    document.body.append(h2);
-    h2.textContent = months[month] + ' ' + year;
-    h2.style.cssText = 'text-align: center;'; 
+    let day = new Date(year, mon);
 
-   
-    let table = document.createElement('table');
-    let trHead = document.createElement('tr');
-
-    // Create th of the table
-    for (i in weekDays) {
-        let th = document.createElement('th');
-        th.textContent = weekDays[i];
-        trHead.append(th);
-        table.append(trHead);
+    // Fill spaces before the first day of that month
+    for (let i = 0; i < getDay(day); i++) {
+        table += '<td></td>';
     }
 
-    let firstDay = new Date(year, month - 1);
-    let dayStart = transform(firstDay.getDay());
+    while (day.getMonth() == mon) {
 
-    let lastDay = new Date(year, month, 0);
-    let dayEnd = transform(lastDay.getDay());
+        table += '<td>' + day.getDate() + '</td>';
+        if (getDay(day) % 7 == 6) {
+            table += '</tr></tr>';
+        }
+        day.setDate(day.getDate() + 1);
+    }
 
-    let numberOfRows = Math.ceil((lastDay.getDate() + dayStart) / 7);
-
-    // Create the table
-    for (let i = 0; i < numberOfRows; i++) {
-        let tr = document.createElement('tr');
-        table.append(tr);
-        for (let j = 0; j < 7; j++) {
-            let td = document.createElement('td');
-            tr.append(td);
+    // Fill spaces after the last day of that month
+    if (getDay(day) != 0) {
+        for (let i = getDay(day); i < 7; i++) {
+            table += '<td></td>';
         }
     }
+    table += '</tr></table>';
 
-    // Fill in numbers based on the first day of the month
-    let firstDayRow = 0;
-    let firstDayCol = dayStart;
-
-    for (let i = 1; i <= lastDay.getDate(); i++) {
-        let temp = i + firstDayCol;
-        table.rows[Math.floor(temp / 7) + 1].cells[temp % 7].textContent = i;
-    }
-
-    table.style.cssText = 'margin:auto; font-size: 20px; padding: 10px; width: 50%; text-align:center';
-    elem.append(table);
+    elem.innerHTML = table;
 }
 
 // The default getDay is starting from Sunday. We need to do some changes.
-function transform(n) {
-    return n >= 1 ? n - 1 : 6;
+function getDay(date) {
+    return date.getDay() >= 1 ? date.getDay() - 1 : 6;
 }
-
-// Do some style ~~ More TBC
-document.body.style.fontFamily = 'Courier New', Courier, monospace;
